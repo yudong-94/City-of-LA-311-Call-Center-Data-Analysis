03c8764653067e9d2217fc05cce5f1ec2fba4edc
hayleezhang <hailey_xw@hotmail.com>
2016-12-06 14:15:16 -0800

    Update server.R

--- Shiny_time/server.R
+++ Shiny_time/server.R
@@ -9,11 +9,14 @@ server <- function(input, output) {
       p <- ggplot(duration_summary, aes(x = reorder(RequestType, -mean), y = mean)) +
         geom_bar(stat = "identity",
                  fill = "paleturquoise3") +
-        ylab("Avg. Processing Time") +
+        ylab("Avg. Processing Time (hrs)") +
         xlab("Request Type") +
         ggtitle("Processing time of different request types") +
         theme_classic() +
-        theme(axis.text.x = element_text(angle = 30, hjust = 1))
+        theme(axis.text.x = element_text(angle = 30, hjust = 1, size = 12),
+              axis.text.y = element_text(size = 14),
+              axis.title = element_text(size=14),
+              title = element_text(size=16, face='bold'))
       print(p)
     }, height = 400, width = 600)
   }) 
@@ -25,7 +28,11 @@ server <- function(input, output) {
         ylab("Number of Requests") +
         xlab("Month of the year") +
         ggtitle("Total Requests by Month") +
-        theme_classic()
+        theme_classic() +
+        theme(axis.text.x = element_text(size = 16),
+              axis.text.y = element_text(size = 16),
+              axis.title = element_text(size=16),
+              title = element_text(size=16, face='bold'))
       print(p) 
     }, height = 400, width = 600)
   }) 
@@ -37,7 +44,11 @@ server <- function(input, output) {
         ylab("Number of Requests") +
         xlab("") +
         ggtitle("Total Requests by Weekday") +
-        theme_classic()
+        theme_classic() +
+        theme(axis.text.x = element_text(size = 16),
+              axis.text.y = element_text(size = 16),
+              axis.title = element_text(size=16),
+              title = element_text(size=16, face='bold'))
       print(p)
     }, height = 400, width = 600)
   })
@@ -53,24 +64,15 @@ server <- function(input, output) {
         theme_classic() +
         ylab("Time of the Day") +
         xlab("") +
-        ggtitle("Total Requests by Weekday and Time of the day")
+        ggtitle("Total Requests by Weekday and Time of the day") +
+        theme(axis.text.x = element_text(size = 16),
+              axis.text.y = element_text(size = 16),
+              axis.title = element_text(size=16),
+              title = element_text(size=16, face='bold'))
       print(p)
     }, height = 400, width = 600)
   })
   
-  observeEvent(input$source_type, {
-    output$plot <- renderPlot({
-      p <- ggplot(request12,aes(x=RequestSource, y=RequestType, fill=Count))+
-        geom_tile()+
-        scale_fill_gradient(low = "mistyrose", high = "deeppink3") +
-        theme_classic() +
-        ylab("") +
-        xlab("") +
-        ggtitle("Requests by Source and Type") +
-        theme(axis.text.x = element_text(angle = 30, hjust = 1))
-      print(p)
-    }, height = 400, width = 600)
-  })
   
   observeEvent(input$source_month, {
     output$plot <- renderPlot({
@@ -84,7 +86,10 @@ server <- function(input, output) {
         ylab("Month of the Year") +
         xlab("") +
         ggtitle("Source of Request and Month of the Year") +
-        theme(axis.text.x = element_text(angle = 30, hjust = 1))
+        theme(axis.text.x = element_text(angle = 30, hjust = 1, size = 12),
+              axis.text.y = element_text(size = 16),
+              axis.title = element_text(size=16),
+              title = element_text(size=16, face='bold'))
       print(p)
     }, height = 400, width = 600)
   })
@@ -101,7 +106,10 @@ server <- function(input, output) {
         ylab("") +
         xlab("") +
         ggtitle("Request Source by Weekday") +
-        theme(axis.text.x = element_text(angle = 30, hjust = 1))
+        theme(axis.text.x = element_text(angle = 30, hjust = 1, size = 12),
+              axis.text.y = element_text(size = 16),
+              axis.title = element_text(size=16),
+              title = element_text(size=16, face='bold'))
       print(p)
     }, height = 400, width = 600)
   })
@@ -118,7 +126,10 @@ server <- function(input, output) {
         ylab("Month of the Year") +
         xlab("") +
         ggtitle("Request Type by Month") +
-        theme(axis.text.x = element_text(angle = 30, hjust = 1))
+        theme(axis.text.x = element_text(angle = 30, hjust = 1, size = 12),
+              axis.text.y = element_text(size = 16),
+              axis.title = element_text(size=16),
+              title = element_text(size=16, face='bold'))
       print(p)
     }, height = 400, width = 600)
   })
@@ -135,7 +146,10 @@ server <- function(input, output) {
         ylab("") +
         xlab("") +
         ggtitle("Type of Request by Weekday") +
-        theme(axis.text.x = element_text(angle = 30, hjust = 1))
+        theme(axis.text.x = element_text(angle = 30, hjust = 1, size = 12),
+              axis.text.y = element_text(size = 16),
+              axis.title = element_text(size=16),
+              title = element_text(size=16, face='bold'))
       print(p)
     }, height = 400, width = 600)
   })
@@ -147,7 +161,11 @@ server <- function(input, output) {
         ylab("Number of Requests") +
         xlab("Month of the year") +
         ggtitle("Calls distribution by Month") +
-        theme_classic()
+        theme_classic() +
+        theme(axis.text.x = element_text(size = 16),
+              axis.text.y = element_text(size = 16),
+              axis.title = element_text(size=16),
+              title = element_text(size=16, face='bold'))
       print(p)
     }, height = 400, width = 600)
   })
@@ -159,9 +177,14 @@ server <- function(input, output) {
         ylab("Number of Requests") +
         xlab("Hour of the day") +
         ggtitle("Calls distribution by hour of the day") +
-        theme_classic()
+        theme_classic() +
+        theme(axis.text.x = element_text(size = 16),
+              axis.text.y = element_text(size = 16),
+              axis.title = element_text(size=16),
+              title = element_text(size=16, face='bold'))
       print(p)
     }, height = 400, width = 600)
   })
   
 }
+
