00064b5ade6b4ecd4d413217d8dd5222252cf203
yudong-94 <yudong@usc.edu>
2016-12-06 17:13:55 -0800

    reorganize time-based analysis

--- Shiny_time/server.R
+++ Shiny_time/server.R
@@ -5,185 +5,43 @@ server <- function(input, output) {
   
   # button
   observeEvent(input$duration, {
-    output$plot <- renderPlot({
-      p <- ggplot(duration_summary, aes(x = reorder(RequestType, -mean), y = mean)) +
-        geom_bar(stat = "identity",
-                 fill = "paleturquoise3") +
-        ylab("Avg. Processing Time (hrs)") +
-        xlab("Request Type") +
-        ggtitle("Processing time of different request types") +
-        theme_classic() +
-        theme(axis.text.x = element_text(angle = 30, hjust = 1, size = 12),
-              axis.text.y = element_text(size = 14),
-              axis.title = element_text(size=14),
-              title = element_text(size=16, face='bold'))
-      print(p)
-    }, height = 400, width = 600)
+    output$plot <- renderPlot(duration_summary_plot(), height = 400, width = 600)
   }) 
   
   observeEvent(input$bymonth, {
-    output$plot <- renderPlot({
-      p <- ggplot(request1, aes(x = factor(month_created))) +
-        geom_bar(fill = "paleturquoise3") +
-        ylab("Number of Requests") +
-        xlab("Month of the year") +
-        ggtitle("Total Requests by Month") +
-        theme_classic() +
-        theme(axis.text.x = element_text(size = 16),
-              axis.text.y = element_text(size = 16),
-              axis.title = element_text(size=16),
-              title = element_text(size=16, face='bold'))
-      print(p) 
-    }, height = 400, width = 600)
+    output$plot <- renderPlot(by_month_plot(), height = 400, width = 600)
   }) 
   
   observeEvent(input$byweek, {
-    output$plot <- renderPlot({
-      p <- ggplot(request1, aes(x = weekday_created)) +
-        geom_bar(fill = "paleturquoise3") +
-        ylab("Number of Requests") +
-        xlab("") +
-        ggtitle("Total Requests by Weekday") +
-        theme_classic() +
-        theme(axis.text.x = element_text(size = 16),
-              axis.text.y = element_text(size = 16),
-              axis.title = element_text(size=16),
-              title = element_text(size=16, face='bold'))
-      print(p)
-    }, height = 400, width = 600)
+    output$plot <- renderPlot(by_weekday_plot(), height = 400, width = 600)
   })
 
   observeEvent(input$weekday_hour, {
-    output$plot <- renderPlot({
-      p <- ggplot(request2, aes(x = weekday_created, 
-                                y = factor(hour_created), 
-                                fill = count)) +
-        geom_tile() +
-        scale_fill_gradient(low = "white", high = "deeppink4", 
-                            breaks = NULL, labels = NULL) +
-        theme_classic() +
-        ylab("Time of the Day") +
-        xlab("") +
-        ggtitle("Total Requests by Weekday and Time of the day") +
-        theme(axis.text.x = element_text(size = 16),
-              axis.text.y = element_text(size = 16),
-              axis.title = element_text(size=16),
-              title = element_text(size=16, face='bold'))
-      print(p)
-    }, height = 400, width = 600)
+    output$plot <- renderPlot(weekday_hour_heatmap(), height = 400, width = 600)
   })
   
-  
   observeEvent(input$source_month, {
-    output$plot <- renderPlot({
-      p <- ggplot(request5, aes(x = RequestSource, 
-                                y = factor(month_created), 
-                                fill = count)) +
-        geom_tile() +
-        scale_fill_gradient(low = "mistyrose", high = "deeppink3", 
-                            breaks = NULL, labels = NULL) +
-        theme_classic() +
-        ylab("Month of the Year") +
-        xlab("") +
-        ggtitle("Source of Request and Month of the Year") +
-        theme(axis.text.x = element_text(angle = 30, hjust = 1, size = 12),
-              axis.text.y = element_text(size = 16),
-              axis.title = element_text(size=16),
-              title = element_text(size=16, face='bold'))
-      print(p)
-    }, height = 400, width = 600)
+    output$plot <- renderPlot(source_month_heatmap(), height = 400, width = 600)
   })
   
   observeEvent(input$source_weekday, {
-    output$plot <- renderPlot({
-      p <- ggplot(request7, aes(x = RequestSource, 
-                                y = factor(weekday_created), 
-                                fill = count)) +
-        geom_tile() +
-        scale_fill_gradient(low = "mistyrose", high = "deeppink3", 
-                            breaks = NULL, labels = NULL) +
-        theme_classic() +
-        ylab("") +
-        xlab("") +
-        ggtitle("Request Source by Weekday") +
-        theme(axis.text.x = element_text(angle = 30, hjust = 1, size = 12),
-              axis.text.y = element_text(size = 16),
-              axis.title = element_text(size=16),
-              title = element_text(size=16, face='bold'))
-      print(p)
-    }, height = 400, width = 600)
+    output$plot <- renderPlot(source_weekday_heatmap(), height = 400, width = 600)
   })
   
   observeEvent(input$type_month, {
-    output$plot <- renderPlot({
-      p <- ggplot(request6, aes(x = RequestType, 
-                                y = factor(month_created), 
-                                fill = count)) +
-        geom_tile() +
-        scale_fill_gradient(low = "mistyrose", high = "deeppink3", 
-                            breaks = NULL, labels = NULL) +
-        theme_classic() +
-        ylab("Month of the Year") +
-        xlab("") +
-        ggtitle("Request Type by Month") +
-        theme(axis.text.x = element_text(angle = 30, hjust = 1, size = 12),
-              axis.text.y = element_text(size = 16),
-              axis.title = element_text(size=16),
-              title = element_text(size=16, face='bold'))
-      print(p)
-    }, height = 400, width = 600)
+    output$plot <- renderPlot(type_month_heatmap(), height = 400, width = 600)
   })
   
   observeEvent(input$type_weekday, {
-    output$plot <- renderPlot({
-      p <- ggplot(request8, aes(x = RequestType, 
-                                y = factor(weekday_created), 
-                                fill = count)) +
-        geom_tile() +
-        scale_fill_gradient(low = "mistyrose", high = "deeppink3", 
-                            breaks = NULL, labels = NULL) +
-        theme_classic() +
-        ylab("") +
-        xlab("") +
-        ggtitle("Type of Request by Weekday") +
-        theme(axis.text.x = element_text(angle = 30, hjust = 1, size = 12),
-              axis.text.y = element_text(size = 16),
-              axis.title = element_text(size=16),
-              title = element_text(size=16, face='bold'))
-      print(p)
-    }, height = 400, width = 600)
+    output$plot <- renderPlot(type_weekday_heatmap(), height = 400, width = 600)
   })
   
   observeEvent(input$calls_month, {
-    output$plot <- renderPlot({
-      p <- ggplot(request9, aes(x = factor(month_created))) +
-        geom_bar(fill = "paleturquoise3") +
-        ylab("Number of Requests") +
-        xlab("Month of the year") +
-        ggtitle("Calls distribution by Month") +
-        theme_classic() +
-        theme(axis.text.x = element_text(size = 16),
-              axis.text.y = element_text(size = 16),
-              axis.title = element_text(size=16),
-              title = element_text(size=16, face='bold'))
-      print(p)
-    }, height = 400, width = 600)
+    output$plot <- renderPlot(call_month_plot(), height = 400, width = 600)
   })
   
   observeEvent(input$calls_hour, {
-    output$plot <- renderPlot({
-      p <- ggplot(request9, aes(x = factor(hour_created))) +
-        geom_bar(fill = "paleturquoise3") +
-        ylab("Number of Requests") +
-        xlab("Hour of the day") +
-        ggtitle("Calls distribution by hour of the day") +
-        theme_classic() +
-        theme(axis.text.x = element_text(size = 16),
-              axis.text.y = element_text(size = 16),
-              axis.title = element_text(size=16),
-              title = element_text(size=16, face='bold'))
-      print(p)
-    }, height = 400, width = 600)
+    output$plot <- renderPlot(call_hour_plot(), height = 400, width = 600)
   })
   
 }
--- Shiny_time/ui.R
+++ Shiny_time/ui.R
@@ -1,59 +1,66 @@
 library(shiny)
 
-ui <- fluidPage(
-  tabsetPanel(
-    tabPanel("Time based analysis",
-  
-  titlePanel("City of LA Requesting Data Analysis - Time Based"),
-  
-  hr(),
-  
-  sidebarPanel(
-
-           actionButton(inputId = "duration",
-                        label = "Avg. Processing Time", width = 200),
-           br(),
-           
-           actionButton(inputId = "bymonth",
-                        label = "Requests by Month", width = 200),
-           br(),
-           
-           actionButton(inputId = "byweek",
-                        label = "Requests by Weekday", width = 200),
-           br(),
-           
-           actionButton(inputId = "weekday_hour",
-                        label = "Weekday and Hour", width = 200),
-           br(),
-           
-           
-           actionButton(inputId = "source_month",
-                        label = "Request Source by Month", width = 200),
-           br(),
-           
-           actionButton(inputId = "source_weekday",
-                        label = "Request Source by Weekday", width = 200),
-           br(),
-           
-           actionButton(inputId = "type_month",
-                        label = "Request Type by Month", width = 200),
-           br(),
-           
-           actionButton(inputId = "type_weekday",
-                        label = "Request Type by Weekday", width = 200),
-           br(),
-           
-           actionButton(inputId = "calls_month",
-                        label = "Calls by Month", width = 200),
-           br(),
-           
-           actionButton(inputId = "calls_hour",
-                        label = "Calls by Hour", width = 200)
+ui <- navbarPage(
+    
+    "Requesting Analysis",
+    
+    fluid = TRUE, 
+    
+    tabPanel("Time-based Analysis",
+             
+             sidebarPanel(
+                 
+                 actionButton(inputId = "duration",
+                              label = "Avg. Processing Time", width = 200),
+                 
+                 br(),
+                 
+                 actionButton(inputId = "bymonth",
+                              label = "Requests by Month", width = 200),
+                 
+                 br(),
+                 
+                 actionButton(inputId = "byweek",
+                              label = "Requests by Weekday", width = 200),
+                 
+                 br(),
+                 
+                 actionButton(inputId = "weekday_hour",
+                              label = "Weekday and Hour", width = 200),
+                 
+                 br(),
+                 
+                 actionButton(inputId = "source_month",
+                              label = "Request Source by Month", width = 200),
+                 
+                 br(),
+                 
+                 actionButton(inputId = "source_weekday",
+                              label = "Request Source by Weekday", width = 200),
+                 
+                 br(),
+                 
+                 actionButton(inputId = "type_month",
+                              label = "Request Type by Month", width = 200),
+                 
+                 br(),
+                 
+                 actionButton(inputId = "type_weekday",
+                              label = "Request Type by Weekday", width = 200),
+                 
+                 br(),
+                 
+                 actionButton(inputId = "calls_month",
+                              label = "Calls by Month", width = 200),
+                 
+                 br(),
+                 
+                 actionButton(inputId = "calls_hour",
+                              label = "Calls by Hour", width = 200)
            ),
-
-
-  mainPanel(
-           plotOutput("plot")
-           )
-  )
-))
+           
+           mainPanel(
+               plotOutput("plot")
+               )
+    )
+)
